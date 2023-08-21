b51a4b6d25f647d86b5326afeab6cd9545e6ff78
bharathtej <chinimillibharath@gmail.com>
2023-08-20 20:36:19 -0500

    second day updated

--- 07_04/script.js
+++ 07_04/script.js
@@ -8,4 +8,21 @@
  * - Remove the item you found using the find method from the array.
  */
 
+const list = ["apple", "mango", "cat", "dog", "elephant", "fish", "giraffe", "monkey"];
+console.log("Build an array with 8 items: ", list);
+list.pop();
+console.log("Remove the last item : ",list);
+list.unshift(list.pop());
+console.log("Add the last item as the first item on the array:", list);
+list.sort();
+console.log("Sort the items by alphabetical order :", list);
+
+let foundItem = list.find(function(item){
+    if(item.length==3) return item;
+});
+
+console.log("Use the find() method to find a specific item in the array :", foundItem);
+
+list.splice(list.indexOf(foundItem), 1);
+console.log("Remove the item you found using the find method from the array : ", list);
 
--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,26 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+
+const content = `
+<ul>
+    <li>Home</li>
+    <li>about</li>
+    <li> test1</li>
+    <li> test2</li>
+    <li>test3</li> 
+</ul>
+
+
+`
+
+const newnav = document.createElement("nav");
+newnav.classList.add("main-navigation");
+newnav.style= "display:flex; flex-direction: row;";
+newnav.innerHTML=content;
+
+const main = document.querySelector(".maincontent");
+main.appendChild(newnav);
+main.insertAdjacentElement('afterbegin', newnav);
+
+
